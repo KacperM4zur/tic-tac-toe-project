61497da8369f8723ce3bf36ce7573aa9586496fa
Kacper <kacper.mazur4@proton.me>
2023-12-14 20:54:57 +0100

    header, players info, player name change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Player from "./components/Player.jsx";
 
 function App() {
-  
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+
+    return <main>
+        <div id="game-container">
+            <ol id="players">
+              <Player initilName="Player 1" symbol="X"/>
+              <Player initilName="Player 2" symbol="O"/>
+            </ol>
+            Game board
+        </div>
+    </main>
 }
 
 export default App
